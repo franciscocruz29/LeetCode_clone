a6011eec96954895a07c72bf5faed7a3fc737661
Francisco Cruz <franciscohuertas25@gmail.com>
2023-06-30 10:38:59 -0500

    Created UI-modal for the sign-in page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,8 +1,9 @@
+import AuthModal from "@/components/Modals/AuthModal";
 import Navbar from "@/components/Navbar/Navbar";
 
-type AuthProps = {};
+type AuthPageProps = {};
 
-const Auth: React.FC<AuthProps> = () => {
+const AuthPage: React.FC<AuthPageProps> = () => {
   return (
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7xl mx-auto">
@@ -10,8 +11,9 @@ const Auth: React.FC<AuthProps> = () => {
         <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
           <img src="/hero.png" alt="Hero image" />
         </div>
+        <AuthModal />
       </div>
     </div>
   );
 };
-export default Auth;
+export default AuthPage;
