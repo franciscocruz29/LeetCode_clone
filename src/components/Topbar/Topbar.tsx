bccdb2123af9b3815445a7fac21f82d8afe3c616
Francisco Cruz <franciscohuertas25@gmail.com>
2023-07-18 11:27:49 -0500

    Completed Topbar update on auth

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
 
 type TopbarProps = {};
 
 const Topbar: React.FC<TopbarProps> = () => {
+  const [user] = useAuthState(auth);
   return (
     <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
       <div
@@ -25,11 +28,32 @@ const Topbar: React.FC<TopbarProps> = () => {
             </a>
           </div>
 
-          <Link href="/auth">
-            <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
-              Sign In
-            </button>
-          </Link>
+          {!user && (
+            <Link href="/auth">
+              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
+                Sign In
+              </button>
+            </Link>
+          )}
+          {user && (
+            <div className="cursor-pointer group relative">
+              <Image
+                src="/avatar.png"
+                alt="Avatar"
+                width={30}
+                height={30}
+                className="rounded-full"
+              />
+              
+              <div
+                className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out"
+              >
+                <p className="text-sm">{user.email}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </nav>
