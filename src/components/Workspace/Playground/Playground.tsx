4190e1fbe9fe677465ff045e3f9a448b20c367fc
Francisco Cruz <franciscohuertas25@gmail.com>
2023-08-09 05:25:17 -0500

    Added Code editor component

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -1,12 +1,35 @@
+import CodeMirror from "@uiw/react-codemirror";
 import PreferenceNav from "./PreferenceNav/PreferenceNav";
+import Split from "react-split";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import { javascript } from "@codemirror/lang-javascript";
 
 type PlaygroundProps = {};
 
 const Playground: React.FC<PlaygroundProps> = () => {
   return (
-    <>
+    <div className="flex flex-col bg-dark-layer-1 relative">
       <PreferenceNav />
-    </>
+
+      <Split
+        className="h-[calc(100vh-94px)]"
+        direction="vertical"
+        sizes={[60, 40]}
+        minSize={60}
+      >
+        <div className="w-full overflow-auto">
+          <CodeMirror
+            value="const a = 1;"
+            theme={vscodeDark}
+            extensions={[javascript()]}
+            style={{ fontSize: 16 }}
+          />
+        </div>
+        <div>
+          tests cases is going to be here
+        </div>
+      </Split>
+    </div>
   );
 };
 export default Playground;
